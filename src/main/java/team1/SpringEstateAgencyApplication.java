109bb4a0d9079e5b4c368c5b757a4ee676e67d2a
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-08-29 16:32:18 +0200

    First Commit,base file

---
+++ src/main/java/team1/SpringEstateAgencyApplication.java
@@ -0,0 +1,13 @@
+package team1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEstateAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEstateAgencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team1/SpringEstateAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package team1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEstateAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
