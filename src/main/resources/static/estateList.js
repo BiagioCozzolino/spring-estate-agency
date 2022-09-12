740f5d5b5fe04fe2023fddcce7a845269e070dca
DavideVerrengiaCaporossi <oren93vc@gmail.com>
2022-09-12 17:55:00 +0200

    aggiustamenti richiesti

---
+++ src/main/resources/static/estateList.js
@@ -0,0 +1,10 @@
+let bottone = document.getElementById('attivaFiltri');
+
+bottone.addEventListener('click', function(e) {
+	e.preventDefault();
+	
+	//alert('ciao');
+	// Se il div filtriAvanzati ha display none -> passo a display block
+	// - come cambiare display elemento tramite javascript
+	// - 
+});
